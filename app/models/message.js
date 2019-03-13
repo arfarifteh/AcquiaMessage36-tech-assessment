37fc40cb7b175211a89aac1107ba8c4f63402615
Alireza Farifteh <arfarifteh@yahoo.com>
2019-03-13 09:07:45 -0400

    Update message.js
    modify date

--- app/models/message.js
+++ app/models/message.js
@@ -5,7 +5,7 @@ export default DS.Model.extend({
     username: DS.attr(),
     useragent: DS.attr(),
     pic: DS.attr(),
-    date: DS.attr(),
+    date: DS.attr('date', { defaultValue() { return new Date(); } }),
     message: DS.attr(),
 
     msgDate: computed('date', () => {
